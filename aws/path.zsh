61da2405a06274f89b160c8bc6fc670025bd55fa
Donny West <donnywest@gmail.com>
2020-04-02 14:34:29 -0400

    update assume role fn

--- aws/path.zsh
+++ aws/path.zsh
@@ -9,17 +9,19 @@ AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
 AWS_DEFAULT_REGION=$(aws configure get region)
 
 function assume_role() {
-  target_account_id=$1
-  role_name=$2
+  account_id=${1}
+  role_name=${2}
   echo "MFA token: "
   read token
-  tmp_json=$(aws sts assume-role --serial-number arn:aws:iam::${DWEST_ACCOUNT_ID}:mfa/dwest --role-arn arn:aws:iam::$target_account_id:role/$role_name --role-session-name 'RoleSession1' --profile default --token-code $token)
-  ec=$?; if [[ $ec == 0 ]]; then
-    export AWS_ACCESS_KEY_ID=$(echo $tmp_json | jq --raw-output '.Credentials.AccessKeyId')
-    export AWS_SECRET_ACCESS_KEY=$(echo $tmp_json | jq --raw-output '.Credentials.SecretAccessKey')
-    export AWS_SESSION_TOKEN=$(echo $tmp_json | jq --raw-output '.Credentials.SessionToken')
+  tmp_json=$(aws sts assume-role --serial-number arn:aws:iam::503818648656:mfa/dwest --role-arn arn:aws:iam::${account_id}:role/$role_name --role-session-name 'RoleSession1' --profile default --token-code $token)
+  ec=$?
+  if [[ $ec = 0 ]]
+  then
+      export AWS_ACCESS_KEY_ID=$(echo $tmp_json | jq --raw-output '.Credentials.AccessKeyId')
+      export AWS_SECRET_ACCESS_KEY=$(echo $tmp_json | jq --raw-output '.Credentials.SecretAccessKey')
+      export AWS_SESSION_TOKEN=$(echo $tmp_json | jq --raw-output '.Credentials.SessionToken')
   else
-    echo "something went wrong assuming role"
+      echo "something went wront assuming role"
   fi
 }
 

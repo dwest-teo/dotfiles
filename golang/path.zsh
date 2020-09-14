626e8ff5c2647014ff82907051a21e285e6504ba
Donny West <donnywest@gmail.com>
2020-09-14 14:35:52 -0400

    Update path.zsh
    update GOROOT for linux

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export GOROOT="/usr/local/opt/go/libexec"
+# export GOROOT="/usr/local/opt/go/libexec"
+export GOROOT="/usr/local/go"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"

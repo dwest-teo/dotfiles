df7ac5f2fb6fe48a3b291ad32ab945b07f629af5
Donny West <donnywest@gmail.com>
2021-07-15 16:18:46 -0400

    try updating goroot for arch

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # export GOROOT="/usr/local/opt/go/libexec"
-export GOROOT="/usr/local/go"
+# export GOROOT="/usr/local/go"
+export GOROOT="/usr/bin/go"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"

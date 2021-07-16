142b18946deda93dd6ec46d4e73873b13483a210
Donny West <donnywest@gmail.com>
2021-07-16 17:13:05 -0400

    comment out goroot for linux

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # export GOROOT="/usr/local/opt/go/libexec"
 # export GOROOT="/usr/local/go"
-export GOROOT="/usr/bin/go"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"

cd4ff83e188e6eaf831736a98ec6e106af991851
Donny West <donnywest@gmail.com>
2021-12-08 20:35:52 -0500

    add logic for home-brew path native + rosetta

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,2 +1,8 @@
 #!/bin/sh
-export PATH="$PATH:/opt/homebrew/"
+if [ "$(arch)" = "arm64" ]; then
+ eval $(/opt/homebrew/bin/brew shellenv);
+ export PATH="$PATH:/usr/local/bin:/usr/local/sbin";
+else
+ eval $(/usr/local/bin/brew shellenv);
+ export PATH="$PATH:/opt/homebrew/bin:/opt/homebrew/sbin";
+fi

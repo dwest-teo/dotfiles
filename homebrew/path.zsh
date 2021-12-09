30fe2c8f8b27eccff3dfcefa0411bd58abd30473
Donny West <donnywest@gmail.com>
2021-12-09 08:25:31 -0500

    vscode settings, move brew path

--- homebrew/path.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$(arch)" = "arm64" ]; then
- eval $(/opt/homebrew/bin/brew shellenv);
- export PATH="$PATH:/usr/local/bin:/usr/local/sbin";
-else
- eval $(/usr/local/bin/brew shellenv);
- export PATH="$PATH:/opt/homebrew/bin:/opt/homebrew/sbin";
-fi

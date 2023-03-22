da73cb236379e38a741f90af990f7aede29c0d12
Donny West <donnywest@gmail.com>
2023-03-22 09:26:53 -0400

    update GOROOT

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export GOROOT="/usr/local/opt/go/libexec"
+# export GOROOT="/usr/local/opt/go/libexec"
+export GOROOT="$(brew --prefix golang)/libexec"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"

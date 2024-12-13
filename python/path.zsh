5d90bd8b0a14a9bbbb2ec0cf5ea8207697607bdf
Donny West <donnywest@gmail.com>
2024-12-13 09:28:53 -0500

    pyenv

--- python/path.zsh
+++ python/path.zsh
@@ -1,2 +1,7 @@
 #!/bin/sh
 export PATH="$PATH:$HOME/.local/bin"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/shims:$PATH"
+
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi

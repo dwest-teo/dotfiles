9968a70ee2222aebeb6495a88483242e8b6ea8bd
Donny West <donnywest@gmail.com>
2024-12-17 17:00:20 -0500

    go config

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,5 +1,14 @@
 #!/bin/sh
 # export GOROOT="/usr/local/opt/go/libexec"
 # export GOROOT="/usr/local/go"
-export GOPATH="$HOME/go"
-export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+
+if [ "$(uname -s)" == "Darwin" ]; then
+  export GOPATH="$HOME/go"
+  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+fi
+
+if [ "$(uname -s)" == "Linux" ]; then
+  export GOPATH="$HOME/go"
+  export GOROOT="/usr/bin/go"
+  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+fi

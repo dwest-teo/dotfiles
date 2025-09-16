c487c2155f9bb4e2d07da9c55e0fcb747333fc3c
Donny West <donnywest@gmail.com>
2025-09-16 13:06:55 -0700

    wsl

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -23,3 +23,26 @@ alias gcd='git checkout development && git pull'
 gi() {
   curl -s "https://www.gitignore.io/api/$*";
 }
+
+# auto start and add CD key in WSL
+env=~/.ssh/agent.env
+
+agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
+
+agent_start () {
+    (umask 077; ssh-agent >| "$env")
+    . "$env" >| /dev/null ; }
+
+agent_load_env
+
+# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
+agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+    agent_start
+    ssh-add ~/.ssh/id_ed25519_2
+elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+    ssh-add ~/.ssh/id_ed25519_2
+fi
+
+unset env
--- golang/path.zsh
+++ golang/path.zsh
@@ -2,13 +2,18 @@
 # export GOROOT="/usr/local/opt/go/libexec"
 # export GOROOT="/usr/local/go"
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  export GOPATH="$HOME/go"
-  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
-fi
+# if [ "$(uname -s)" == "Darwin" ]; then
+#   export GOPATH="$HOME/go"
+#   export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+# fi
 
-if [ "$(uname -s)" == "Linux" ]; then
-  export GOPATH="$HOME/go"
-  export GOROOT="/usr/bin/go"
-  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
-fi
+# if [ "$(uname -s)" == "Linux" ]; then
+#   export GOPATH="$HOME/go"
+#   export GOROOT="/usr/bin/go"
+#   export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+# fi
+
+# temp to work with WSL, figure out why the uname check didn't work
+export GOPATH="$HOME/go"
+export GOROOT="/usr/bin/go"
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"

c5dbb006dbeba4ad62719e88e624a8da4614e244
Donny West <donnywest@gmail.com>
2025-09-18 17:24:27 -0700

    Update path.zsh

--- antibody/path.zsh
+++ antibody/path.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
-source $HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh
-
-antibody() {
-  eval "$(command antibody init -)"
-  antibody "$@"
-}
+# antibody() {
+#   eval "$(command antibody init -)"
+#   antibody "$@"
+# }

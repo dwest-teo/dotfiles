c88c4b7827ac84c4fa1561bab5b3b50040459c37
Donny West <donnywest@gmail.com>
2025-09-18 17:26:42 -0700

    Update install.sh

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
+
 source $HOMEBREW_PREFIX/opt/antidote/share/antidote/antidote.zsh
 
 antidote bundle < "$ZSH/antibody/pre_prompt.txt" > ~/.antibody_bundles.txt
